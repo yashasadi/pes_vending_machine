4e5dcbc0a2fa2aa37d2f906c03e6ad1010972297
yashasadi <109977509+yashasadi@users.noreply.github.com>
2023-09-01 14:27:24 +0530

    Create pes_vm_tb.v

---
+++ pes_vm_tb.v
@@ -0,0 +1,34 @@
+module pes_vm_tb;
+
+  //inputs
+  reg clk;
+  reg[1:0] in;
+  reg rst;
+
+  //output
+  wire out;
+  wire[1:0] change;
+
+ iiitb_vm uut(
+  .clock(clk),
+  .reset(rst),
+  .in(in),
+  .out(out),
+  .change(change)
+ );
+
+ initial begin
+   //initialise inputs
+   $dumpfile("pes_vm.vcd");
+   $dumpvars(0,pes_vm_tb);
+  rst = 1;
+  clk = 0;
+
+  #6 rst = 0;
+  in = 1;
+  #19 in = 2;
+  #25 $finish;
+
+ end
+ always #5 clk = ~clk;
+endmodule
